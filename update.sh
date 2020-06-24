d03f9f6935f98fcd1388c3e5057e4f5982256cae
John Regehr <regehr@cs.utah.edu>
2020-06-24 16:55:18 +0000

    foo

--- update.sh
+++ update.sh
@@ -1,14 +1,17 @@
 set -e
 
+(
 cd $HOME/z3
 git pull
 rm -rf build
 rm -rf $HOME/z3-install
-CXX=clang++ CC=clang python scripts/mk_make.py --prefix=$HOME/z3-install
+CXX=clang++ CC=clang python scripts/mk_make.py
 cd build
-make -j32
-make install
+make -j4
+sudo make install
+)
 
+(
 cd $HOME/llvm-project
 git pull
 rm -rf build
@@ -18,7 +21,9 @@ cmake -G Ninja -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_EH=ON -DLLVM_BUILD_LLVM_DYLIB
 ninja
 ninja check
 ninja install
+)
 
+(
 cd $HOME/alive2
 git pull
 rm -rf build-new
@@ -27,8 +32,7 @@ cd build-new
 cmake .. -G Ninja -DCLANG_PLUGIN=1 -DBUILD_TV=1 -DBUILD_LLVM_UTILS=1 -DLLVM_DIR=$HOME/llvm-install/lib/cmake/llvm -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
 ninja
 ninja check || true # some cases always fail
-
-cd $HOME
+)
 
 echo
 echo
