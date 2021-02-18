d35417a9000b3322c9a5fb808041dbec2a05c0b5
John Regehr <regehr@cs.utah.edu>
2021-02-18 02:26:02 +0000

    foo

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ cd $HOME/z3
 git pull
 rm -rf build
 rm -rf $HOME/z3-install
-CXX=clang++ CC=clang python scripts/mk_make.py
+python scripts/mk_make.py
 cd build
 make -j4
 sudo make install
@@ -13,23 +13,24 @@ sudo make install
 
 (
 cd $HOME/llvm-project
-git pull
+git pull || true
 rm -rf build
 mkdir build
 cd build
-cmake -G Ninja -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_EH=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release  -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX=$HOME/llvm-install ../llvm -DLLVM_ENABLE_PROJECTS="clang"
+cmake -G Ninja -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_EH=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release  -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_INSTALL_PREFIX=$HOME/llvm-install ../llvm -DLLVM_ENABLE_PROJECTS="llvm;lld;clang;compiler-rt;libcxx;libcxxabi"
 ninja
 ninja check
 ninja install
 )
 
 (
+export LD_LIBRARY_PATH=/home/ce/llvm-project/build/lib:$LD_LIBRARY_PATH
 cd $HOME/alive2
 git pull
 rm -rf build-new
 mkdir build-new
 cd build-new
-cmake .. -G Ninja -DCLANG_PLUGIN=1 -DBUILD_TV=1 -DBUILD_LLVM_UTILS=1 -DLLVM_DIR=$HOME/llvm-install/lib/cmake/llvm -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
+cmake .. -G Ninja -DBUILD_TV=1 -DBUILD_LLVM_UTILS=1 -DLLVM_DIR=$HOME/llvm-install/lib/cmake/llvm -DCMAKE_BUILD_TYPE=Release
 ninja
 ninja check || true # some cases always fail
 )
